3d0e3bff59b86e028ff70754117e049f7c331b92
Ravil <newromeo2006@yandex.ru>
2024-03-11 19:56:52 +0300

    HW_4

--- myproject/urls.py
+++ myproject/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('myapp.urls')),
     path('myapp3/', include('myapp3.urls')),
+    path('myapp4/', include('myapp4.urls')),
 ]
