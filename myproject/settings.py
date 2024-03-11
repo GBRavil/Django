43e33a3951cb4429854d0252e70206f7efe2b211
Ravil <newromeo2006@yandex.ru>
2024-03-11 20:43:49 +0300

    HW_6.2

--- myproject/settings.py
+++ myproject/settings.py
@@ -20,15 +20,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*rz*$^jgr8pyxe%u!(b&3z!nf&xr$4hyz7m2y-n5@_-hyeafv#'
+# SECRET_KEY = 'django-insecure-*rz*$^jgr8pyxe%u!(b&3z!nf&xr$4hyz7m2y-n5@_-hyeafv#'
+import os
+
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
-    "192.168.0.109",
-
+    # '192.168.0.109',
+    'rav24.pythonanywhere.com',
 ]
 
 INTERNAL_IPS = [
@@ -89,8 +94,15 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'rav24$default',
+        'USER': 'rav24',
+        'PASSWORD': os.getenv("MYSQL_PASSWORD"),
+        'HOST': 'rav24.mysql.pythonanywhere-services.com',
+        'OPTIONS': {
+            'init_command': "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
     }
 }
 
@@ -130,6 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
